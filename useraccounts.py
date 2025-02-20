f5ccab2bffe090cc56c521d65bb2e0cd0568493b
Marie Kolawole <marie.kolawole@georgetown.edu>
2025-02-20 05:04:52 -0500

    Update route functions to use database session

--- useraccounts.py
+++ useraccounts.py
@@ -1,13 +1,14 @@
 from contextlib import asynccontextmanager
 from datetime import date
-from typing import List, Any, Coroutine, Sequence
+from typing import List, Any, Coroutine, Sequence, Annotated
 
 import uvicorn
 from fastapi import FastAPI, HTTPException
+from fastapi.params import Depends
 from sqlalchemy import create_engine
 from sqlmodel import SQLModel, Session, select
 
-from schema import User, load_users, save_users, UserCreate, UserUpdate
+from schemas import User, load_users, save_users, UserCreate, UserUpdate
 
 # Database Setup
 # Defines the SQLite database file location
@@ -22,6 +23,9 @@ async def lifespan(app: FastAPI):
     SQLModel.metadata.create_all(engine) # creates the database tables before requests are processed
     yield
 
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 app = FastAPI(
     title="User Account API",
@@ -31,68 +35,63 @@ app = FastAPI(
 
 
 @app.get("/users", responses={200: {"model": List[User], "description": "A list of user accounts"}})
-async def list_users() -> Sequence[User]:
+async def list_users(session: Annotated[Session, Depends(get_session)]) -> Sequence[User]:
     """
     Retrieves a list of user accounts
     """
-    with Session(engine) as session:
-        query = select(User)
-        return session.exec(query).all()
+    query = select(User)
+    return session.exec(query).all()
 
 
 @app.post("/users", responses={201: {"model": User, "description": "User account created successfully"}})
-async def create_user(user: UserCreate) -> User:
+async def create_user(session: Annotated[Session, Depends(get_session)], user: UserCreate) -> User:
     """
     Create a new user account
     """
-    with Session(engine) as session: # wraps in db transaction
-        new_user = User.model_validate(user)
-        session.add(new_user)
-        session.commit()
-        session.refresh(new_user) # updates the object with the database generated id
-        return new_user
+    new_user = User.model_validate(user)
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user) # updates the object with the database generated id
+    return new_user
 
 
 @app.get("/users/{userId}", responses={200: {"model": User, "description": "A user account"}, 404: {"description": "User not found"}})
-async def get_user(user_id: int) -> User:
+async def get_user(session: Annotated[Session, Depends(get_session)], user_id: int) -> User:
     """
     Retrieve a user account by ID
     """
     users = select(User, User.id == user_id)
-    with Session(engine) as session:
-        user = session.exec(users).first()
-        return user[0] if user else HTTPException(status_code=404, detail=f"No user with id={user_id}.")
+    user = session.exec(users).first()
+    return user[0] if user else HTTPException(status_code=404, detail=f"No user with id={user_id}.")
 
 
-#
-# @app.put("/users/{userId}", responses={200: {"model": User, "description": "User account updated  successfully"}, 404: {"description": "User not found"}})
-# async def update_user(user_id: int, updated_user: UserUpdate) -> User:
-#     """
-#     Update an existing user account
-#     """
-#
-#     for i, user in enumerate(db):
-#         if user.id == user_id:
-#             db[i] = User( id=user_id, **updated_user.model_dump())
-#             save_users(db)
-#             return db[i]
-#
-#     raise HTTPException(status_code=404, detail=f"No user with id={user_id}.")
-#
-#
-# @app.delete("/users/{userId}", status_code=204, responses={204: {"description": "User account deleted successfully"}, 404: {"description": "User not found"}})
-# def delete_user(user_id: int):
-#     """
-#     Delete a user account
-#     """
-#
-#     for i, user in enumerate(db):
-#         if user.id == user_id:
-#             db.pop(i)
-#             save_users(db)
-#             return
-#
-#     raise HTTPException(status_code=404, detail=f"No user with id={user_id}.")
+
+@app.put("/users/{userId}", responses={200: {"model": User, "description": "User account updated  successfully"}, 404: {"description": "User not found"}})
+async def update_user(session: Annotated[Session, Depends(get_session)], user_id: int, updated_user: UserUpdate) -> User:
+    """
+    Update an existing user account
+    """
+    user = session.get(User, user_id)
+    if user:
+        user.sqlmodel_update(updated_user)
+        session.commit()
+        session.refresh(user)
+        return User.model_validate(user)
+    else:
+        raise HTTPException(status_code=404, detail=f"No user with id={user_id}.")
+
+
+@app.delete("/users/{userId}", status_code=204, responses={204: {"description": "User account deleted successfully"}, 404: {"description": "User not found"}})
+def delete_user(session: Annotated[Session, Depends(get_session)], user_id: int):
+    """
+    Delete a user account
+    """
+    user = session.get(User, user_id)
+    if user:
+         session.delete(user)
+         session.commit()
+    else:
+        raise HTTPException(status_code=404, detail=f"No user with id={user_id}.")
 
 if __name__ == "__main__":
     uvicorn.run("useraccounts:app", reload=True)
